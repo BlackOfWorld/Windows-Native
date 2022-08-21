57408a11b4f953a353c928e83321e844db66c630
BlackOfWorld <admin@blackofworld.fun>
2022-08-21 22:11:00 +0200

    tweak: Build batch file now supports arguments
    Signed-off-by: BlackOfWorld <admin@blackofworld.fun>

--- build/premake_win.bat
+++ build/premake_win.bat
@@ -1,1 +1,11 @@
-bin\win\premake5 --file=..\premake5.lua vs2022
+@echo off
+set "arg=%~1"
+
+rem Custom clean function, because premake doesn't support clean yet
+if "%arg%"=="clean" (rmdir /s /q ..\out & exit /b 0)
+
+rem Default argument is vs2022
+if "%arg%"=="" (set "arg=vs2022")
+
+bin\win\premake5 --file=..\premake5.lua %arg%
+@echo on
