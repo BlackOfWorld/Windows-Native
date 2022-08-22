710e868d535c804d95989abedbbe0da1619a2d64
BlackOfWorld <admin@blackofworld.fun>
2022-08-22 12:25:26 +0200

    Fix CLI for the last time!
    Signed-off-by: BlackOfWorld <admin@blackofworld.fun>

--- build/premake_win.bat
+++ build/premake_win.bat
@@ -6,6 +6,9 @@ pushd %~dp0
 rem Custom clean function, because premake doesn't support clean yet
 if "%arg%"=="clean" (rmdir /s /q ..\out & exit /b 0)
 
+rem Custom vcvarsall function for CLI builds
+if "%arg%"=="vcvarsall" (bin\win\vswhere.exe -find VC\Auxiliary\Build\vcvarsall.bat & exit /b 0)
+
 rem Default argument is vs2022
 if "%arg%"=="" (set "arg=vs2022")
 
