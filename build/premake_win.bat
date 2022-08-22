2308e5f94da66f5e90fcfe75795c309ba2a92665
BlackOfWorld <admin@blackofworld.fun>
2022-08-22 12:20:20 +0200

    fix: Not setting correct path for premake batch file
    Signed-off-by: BlackOfWorld <admin@blackofworld.fun>

--- build/premake_win.bat
+++ build/premake_win.bat
@@ -1,6 +1,8 @@
 @echo off
 set "arg=%~1"
 
+pushd %~dp0
+
 rem Custom clean function, because premake doesn't support clean yet
 if "%arg%"=="clean" (rmdir /s /q ..\out & exit /b 0)
 
@@ -8,4 +10,6 @@ rem Default argument is vs2022
 if "%arg%"=="" (set "arg=vs2022")
 
 bin\win\premake5 --file=..\premake5.lua %arg%
+
+popd
 @echo on
